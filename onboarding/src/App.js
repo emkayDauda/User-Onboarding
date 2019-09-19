4ba803c5c4471f09f42d5d8b5d0710e71cc76ee6
Maaruf Dauda <mkdauda.work@gmail.com>
2019-09-19 14:54:01 +0100

    :sparkles: Add uuid

--- onboarding/src/App.js
+++ onboarding/src/App.js
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import "./App.css";
 import CustomForm from "./components/Form";
 import axios from "axios";
+import uuid from 'uuid';
 
 function App() {
   const [postedUsers, setPostedUsers] = useState([]);
 
   function submit(formValues, actions) {
     const usertoPost = {
-      id: 1,
+      id: uuid(),
       email: formValues.email,
       first_name: formValues.name.split(" ")[0],
       last_name: formValues.name.split(" ")[1],
@@ -31,7 +32,7 @@ function App() {
     <div className="App">
       <CustomForm onSubmit={submit} />
       {postedUsers.map(user => (
-        <h3>{user.first_name}</h3>
+        <h3 key={user.id}>{user.first_name}</h3>
       ))}
     </div>
   );
