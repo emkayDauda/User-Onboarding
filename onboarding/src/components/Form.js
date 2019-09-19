8e26fb55f287f0ff3273b699f206eaa8db7d20c6
Maaruf Dauda <mkdauda.work@gmail.com>
2019-09-19 13:23:53 +0100

    :sparkles: Add validation schema

--- onboarding/src/components/Form.js
+++ onboarding/src/components/Form.js
@@ -1,28 +1,44 @@
 import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
 
 export default function CustomForm(){
+
+    const validationSchema = yup.object().shape({
+        name: yup.string()
+            .required('Name is required'),
+        email: yup.string()
+            .required('Email is required'),
+        password: yup.string()
+            .required('password is required'),
+        terms: yup.boolean()
+            .required()
+            .isType(true)
+            
+    })
+
     return (
       <Formik
+      validationSchema={validationSchema}
         render={props => {
           return (
             <Form>
               <div>
-                <Field name="a" type="text" placeholder="Name" />
-                <ErrorMessage name="a" component="div" />
+                <Field name="name" type="text" placeholder="Name" />
+                <ErrorMessage name="name" component="div" />
               </div>
               <div>
-                <Field name="b" type="text" placeholder="Email" />
-                <ErrorMessage name="b" component="div" />
+                <Field name="email" type="text" placeholder="Email" />
+                <ErrorMessage name="email" component="div" />
               </div>
               <div>
-                <Field name="c" type="text" placeholder="Password" />
-                <ErrorMessage name="c" component="div" />
+                <Field name="password" type="text" placeholder="Password" />
+                <ErrorMessage name="password" component="div" />
               </div>
               <div>
                 <label>
-                    <Field name="d" type="checkbox" placeholder="" />
-                    <ErrorMessage name="d" component="div" />
+                    <Field name="terms" type="checkbox" placeholder="" />
+                    <ErrorMessage name="terms" component="div" />
                     Read and accepted terms of service.
                 </label>
               </div>
