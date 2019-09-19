496f471be2b47880fffd5491cc901ef46f52d0d2
Maaruf Dauda <mkdauda.work@gmail.com>
2019-09-19 12:42:48 +0100

    :sparkles: Create and render empty form

---
+++ onboarding/src/components/Form.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
+export default function CustomForm(){
+    return(
+        <Formik
+          render ={props => {
+              return (
+                  <Form>
+                      
+                  </Form>
+              )
+          }}
+        
+        />
+
+    
+    );
+}
