8b3a478407712cfa75034a80d064f2d39b36d6f0
Maaruf Dauda <mkdauda.work@gmail.com>
2019-09-19 14:43:15 +0100

    :art: Clear form on submission done

--- onboarding/src/App.js
+++ onboarding/src/App.js
@@ -1,32 +1,38 @@
-import React, {useState} from 'react';
-import './App.css';
-import CustomForm from './components/Form'
-import axios from 'axios';
+import React, { useState } from "react";
+import "./App.css";
+import CustomForm from "./components/Form";
+import axios from "axios";
 
 function App() {
-  const [postedUsers, setPostedUsers] = useState([])
+  const [postedUsers, setPostedUsers] = useState([]);
 
-  function submit(formValues, actions){
+  function submit(formValues, actions) {
     const usertoPost = {
       id: 1,
       email: formValues.email,
       first_name: formValues.name.split(" ")[0],
       last_name: formValues.name.split(" ")[1],
-      avatar: ""
-
-  }
-  axios.post("https://reqres.in/api/users", usertoPost)
-  .then(response => {
-      console.log(response.data);
-  })
-  .catch(err => {
-      console.log(err);
-      
-  })
+      password: formValues.password
+    };
+    axios
+      .post("https://reqres.in/api/users", usertoPost)
+      .then(response => {
+        console.log(response.data);
+        const newUsers = postedUsers.concat(response.data);
+        setPostedUsers(newUsers);
+        console.log(postedUsers);
+        actions.resetForm();
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
   return (
     <div className="App">
-      <CustomForm onSubmit={submit}/>
+      <CustomForm onSubmit={submit} />
+      {postedUsers.map(user => (
+        <h3>{user.first_name}</h3>
+      ))}
     </div>
   );
 }
